f4aac8032d4a3dfe0ee23c139b6657f6d6f5847d
Avinash-9702 <186063762+Avinash-9702@users.noreply.github.com>
2024-10-24 20:47:01 +0530

    1.3 version

--- src/test/java/TestNG/NewTest.java
+++ src/test/java/TestNG/NewTest.java
@@ -1,9 +1,23 @@
 package TestNG;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
 public class NewTest {
   @Test
   public void f() {
+	  WebDriver w = new ChromeDriver();
+	  w.get("https://demo.frontaccounting.eu/");
+		
+		w.findElement(By.name("user_name_entry_field")).clear();
+		w.findElement(By.name("user_name_entry_field")).sendKeys("demouser");
+		
+		w.findElement(By.name("password")).clear();
+		w.findElement(By.name("password")).sendKeys("password");
+		
+		w.findElement(By.name("SubmitUser")).click();
+	  
   }
 }
