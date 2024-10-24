1cc0b302cbfc44e81a722ce0050fe4620d04556b
Avinash-9702 <186063762+Avinash-9702@users.noreply.github.com>
2024-10-24 20:35:46 +0530

    1.2 version

--- src/test/java/TestNG/CustomizationReport.java
+++ src/test/java/TestNG/CustomizationReport.java
@@ -1,5 +1,6 @@
 package TestNG;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Reporter;
@@ -10,7 +11,15 @@ public class CustomizationReport {
 	public void TestDemo() {
 		WebDriver w = new ChromeDriver();
 		Reporter.log("Chrome has been Launched");
-		w.get("");
+		w.get("https://demo.frontaccounting.eu/");
+		
+		w.findElement(By.name("user_name_entry_field")).clear();
+		w.findElement(By.name("user_name_entry_field")).sendKeys("demouser");
+		
+		w.findElement(By.name("password")).clear();
+		w.findElement(By.name("password")).sendKeys("password");
+		
+		w.findElement(By.name("SubmitUser")).click();
 		
 		
 	}
